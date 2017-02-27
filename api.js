cd3619a959500c94e3b802e12f34fb857a8beb4c
Maarten Blokker <maarten.blokker@outlook.com>
2017-02-27 15:23:14 +0100

    made the form actually post registration to CERES

---
+++ api.js
@@ -0,0 +1,36 @@
+var request = require('request');
+
+const api_host = 'ceres-all.sit.debijenkorf.nl';
+const api_port = '80';
+const path_register = 'http://ceres-all.sit.debijenkorf.nl:80/customer/user/register';
+
+const headers = {
+    'User-Agent': 'Mock Agent/0.0.1',
+    'Content-Type': 'application/x-www-form-urlencoded'
+};
+
+module.exports = {
+    register: function (customer, callback) {
+        var data = JSON.parse(JSON.stringify(customer));
+        data['api_version'] = '2.13';
+
+        console.log('sending registration data: ' + JSON.stringify(data, null, 2));
+
+        // Configure the request
+        const options = {
+            url: path_register,
+            method: 'POST',
+            headers: headers,
+            form: data
+        };
+
+        // Start the request
+        request(options, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                callback(null, response);
+            } else {
+                callback(JSON.parse(body).error, response);
+            }
+        });
+    }
+};
--- routes/register.js
+++ routes/register.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var api = require('../api');
 
 /* GET home page. */
 router.get('/', function (req, res) {
@@ -7,10 +8,39 @@ router.get('/', function (req, res) {
 });
 
 router.post('/', function (req, res) {
-    console.log('received post: ');
-    console.log(JSON.stringify(req.body, null, 2));
 
-    res.redirect('/register');
+
+    const isCardMember = +req.body.radioCreditcard;
+    const password = Buffer.from(req.body.password).toString('base64');
+    const customer = {
+        'email': req.body.email,
+        'password': password,
+        'firstName': req.body.firstName,
+        'preposition': req.body.secondName,
+        'lastName': req.body.lastName
+    };
+    if (isCardMember) {
+        customer['card'] = {
+            'expiryMonth': req.body.expiryMonth,
+            'expiryYear': req.body.expiryMonth,
+            'cardHolderName': req.body.cardHolderName,
+            'cardNumber': +req.body.cardNumber,
+            'cvc': +req.body.cvc
+        };
+    }
+
+    console.log('Registering user: ');
+    console.log(JSON.stringify(customer, null, 2));
+
+    api.register(customer, function (err, response) {
+        if (err) {
+            console.log('Failed to register user: ' + JSON.stringify(err, null, 2));
+        } else {
+            console.log('Sucessfully registered user: ' + JSON.stringify(response.toJSON(), null, 2));
+        }
+
+        res.redirect('/register');
+    });
 });
 
 module.exports = router;
